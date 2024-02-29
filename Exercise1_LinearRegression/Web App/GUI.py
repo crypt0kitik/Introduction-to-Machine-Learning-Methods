835af80e7d3c72b7e4fde3a0a1605a7434db2b42
crypt0kitik <emiliiazemskova@icloud.com>
2024-02-29 16:10:23 +0200

    updated

--- Exercise1_LinearRegression/Web App/GUI.py
+++ Exercise1_LinearRegression/Web App/GUI.py
@@ -1,17 +1,43 @@
 # import libraries
 from tkinter import *
-
 import customtkinter
 import customtkinter, tkinter
 import tkinter as tk
+import PIL
+from PIL import Image
+from PIL import ImageTk
 
 # create a window
 customtkinter.set_appearance_mode("dark")
 root = customtkinter.CTk()
 root.geometry("700x400")
 
-# place text in front of pictures
-text_label = tk.Label(root, text="Imagine that you want to develop a movie, \n but beofre you want to know whether is a movie going to be popular", font=("Helvetica", 22), fg="black", bg="white")
+image = PIL.Image.open("Image.png")
+background_image = customtkinter.CTkImage(image, size=(700, 400))
+# Define a function to resize the image
+def resize_image(event=None):
+    new_width = root.winfo_width()
+    new_height = root.winfo_height()
+    # if you have issues with ANTIALIAS
+    # you need Pillow==9.5.0 version
+    resized_image = image.resize((new_width, new_height), Image.ANTIALIAS)
+    photo = ImageTk.PhotoImage(resized_image)
+    bg_lbl.configure(image=photo)
+    bg_lbl.image = photo  # Keep a reference to avoid garbage collection
+
+# Create a background label with the initial image
+bg_lbl = customtkinter.CTkLabel(root, text="", width=700, height=400)
+bg_lbl.place(x=0, y=0)
+
+# Call the resize_image function when the window is resized
+root.bind("<Configure>", resize_image)
+
+# Initialize the image size
+resize_image()
+
+# place text label
+text_label = tk.Label(root, text="Imagine that you want to develop a movie, \n but before you want to know whether the movie is going to be popular",
+                      font=("Helvetica", 22), fg="white", bg="black")  # Set background to transparent
 text_label.place(relx=0.5, rely=0.1, anchor="center")
 
 ################
@@ -24,10 +50,6 @@ combobox = customtkinter.CTkOptionMenu(master=root,
                                        command=optionmenu_callback,
                                        variable=optionmenu_var)
 combobox.pack(padx=20, pady=80)
-
-#########
-progressbar = customtkinter.CTkProgressBar(master=root)
-progressbar.pack(padx=20, pady=90)
 
 #########
 button = customtkinter.CTkButton(master=root, text="Check potential rating")
